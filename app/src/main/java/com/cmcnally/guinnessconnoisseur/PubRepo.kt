a3ec2ff3d0919eed5431f1f7ec548c9a1d67326c
Ciaran McNally <ciaran.mcnally94@gmail.com>
2019-04-10 22:17:26 +0100

    PubRepo implemented

--- app/src/main/java/com/cmcnally/guinnessconnoisseur/PubRepo.kt
+++ app/src/main/java/com/cmcnally/guinnessconnoisseur/PubRepo.kt
@@ -2,7 +2,24 @@ package com.cmcnally.guinnessconnoisseur
 
 
 
-class PubRepo(private val pubService: PubService) {
+class PubRepo(private val pubService: PubService): PubService.DataListener{
 
+    var listener: RepoListener? = null
 
+    init {
+        pubService.listener = this
+    }
+
+    fun getPubs(latitude: String, longitude: String){
+
+        pubService.fetchData(latitude, longitude)
+    }
+
+    override fun onResponseReceived(response: Pubs) {
+        listener?.returnPubs(response)
+    }
+
+    interface RepoListener {
+        fun returnPubs(pubs: Pubs)
+    }
 }
